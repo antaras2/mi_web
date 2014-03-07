3391653924a07a1cc11061917024fb9882fa9fa9
DANIEL <DANIEL@192.168.1.73>
2014-03-07 17:28:26 -0600

    Primeros Archivos subidos a Github.com

---
+++ CrearGit/src/creargit/CrearGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package creargit;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class CrearGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       NuevaClase clase1 = new NuevaClase();
+        System.out.println("generar Git");
+        clase1.clase();
+    
+    }
+    
+}
---
+++ CrearGit/src/creargit/NuevaClase.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package creargit;
+
+/**
+ *
+ * @author DANIEL
+ */
+class NuevaClase {
+   nuevaClase1 clases = new nuevaClase1();
+
+    void clase() {
+         System.out.println("generar Git");
+    }
+    private static class nuevaClase1 {
+
+        public nuevaClase1() {
+             System.out.println("generar Git");
+        }
+    }
+   
+}
